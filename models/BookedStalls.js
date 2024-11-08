2567f360deebce1a3cf992848bd3f261d4061e6b
vrindaborkar <vrindaborkar15@gmail.com>
2024-11-08 20:27:56 +0530

    Date added in schema

--- models/BookedStalls.js
+++ models/BookedStalls.js
@@ -3,15 +3,16 @@ const mongoose = require("mongoose");
 const BookedStalls = mongoose.model(
   "BookedStalls",
   new mongoose.Schema({
-      location:String,
-      address:String,
+      location: String,
+      address: String,
       stallName: String,
-      link:String,
+      link: String,
       stallPrice: Number,
       isBooked: Boolean,
       bookedBy: String,
       bookedAt: String,
-      stallNo:Number
+      stallNo: Number,
+      date: String // new field to store booking date
   },{
     bufferCommands: true,
     autoCreate: false 
@@ -19,35 +20,3 @@ const BookedStalls = mongoose.model(
 );
 
 module.exports = BookedStalls;
-
-
-// const mongoose = require("mongoose");
-
-// const BookedStalls = mongoose.model(
-//   "BookedStalls",
-//   new mongoose.Schema({
-//       stallId: {
-//           type: mongoose.Schema.Types.ObjectId,
-//           ref: 'Stalls',  // Reference to the Stalls schema
-//           required: true
-//       },
-//       isBooked: {
-//           type: Boolean,
-//           default: false
-//       },
-//       bookedBy: {
-//           type: String,   // This can be an ObjectId if referencing a user schema
-//           required: true
-//       },
-//       bookedAt: {
-//           type: Date,
-//           default: Date.now
-//       }
-//   },{
-//     bufferCommands: true,
-//     autoCreate: false 
-//   })
-// );
-
-// module.exports = BookedStalls;
-// const mongoose = require("mongoose");
