89f38643a8ef108ac35b45af1ff6c1a302c02486
kapilbelure <kapil.belure@multigenesys.com>
2024-11-25 18:46:35 +0530

    Added terms popup to accept before payment

--- client/src/components/market/index.js
+++ client/src/components/market/index.js
@@ -71,7 +71,7 @@ const MarketComponent = (props) => {
     const city = e.value;
     setSelectedCity(city);
     setLoading(true);
-    
+
     const marketsInCity = marketList[city] || [];
     setFilteredMarkets(marketsInCity);
     setCity(city);
@@ -114,92 +114,95 @@ const MarketComponent = (props) => {
         <ProgressSpinner />
       ) : (
         <>
-      {shouldRenderFullPageError() && <ErrorPage />}
-      {shouldRenderNotFoundView() && <AccessDeniedPage />}
-      {shouldRenderMarketList() && (
-        <div className="text-center mt-3 px-5">
-          <div className="">
-            <Link to="/farmer" className="text-d-none">
-              <Button
-                className="p-button-rounded flex justify-content-start"
-                icon="pi pi-angle-left mr-2"
-              >
-                {t("back")}
-              </Button>
-            </Link>
-            <Dropdown
-              value={selectedState}
-              options={states}
-              onChange={handleStateChange}
-              placeholder="Select a State"
-              className="m-3"
-            />
+          {shouldRenderFullPageError() && <ErrorPage />}
+          {shouldRenderNotFoundView() && <AccessDeniedPage />}
+          {shouldRenderMarketList() && (
+            <div className="text-center mt-3 px-5">
+              <div className="">
+                <div className="text-left">
+                <div className="d-inline-block"> 
+                  <Link to="/farmer" className="text-d-none">
+                    <Button
+                      className="p-button-rounded flex justify-content-start"
+                      icon="pi pi-angle-left mr-2"
+                    >
+                      {t("back")}
+                    </Button>
+                  </Link>
+                </div>
+                </div>
+                <Dropdown
+                  value={selectedState}
+                  options={states}
+                  onChange={handleStateChange}
+                  placeholder="Select a State"
+                  className="m-3"
+                />
 
-            {selectedState && (
-              <Dropdown
-                value={selectedCity}
-                options={cities[selectedState] || []}
-                onChange={handleCityChange}
-                placeholder="Select a City"
-              />
-            )}
+                {selectedState && (
+                  <Dropdown
+                    value={selectedCity}
+                    options={cities[selectedState] || []}
+                    onChange={handleCityChange}
+                    placeholder="Select a City"
+                  />
+                )}
 
-            {selectedState && selectedCity ? (
-              <>
-                <h2 className="mt-3">
-                  {t(
-                    `select_market_in_${
-                      cities[selectedState].find(
-                        (city) => city.value === selectedCity
-                      )?.label
-                    }`
-                  )}
-                </h2>
-                <div className="grid md:px-5 py-3">
-                  {filteredMarkets.length > 0 ? (
-                    filteredMarkets.map((market, index) => (
-                      <div
-                        key={index}
-                        className="col-12 md:col-6 lg:col-3 cursor-pointer"
-                        onClick={() => handleMarket(market)}
-                      >
-                        <div className="surface-0 shadow-1 p-3 border-1 border-50 border-round h-full hover:shadow-5">
-                          <div className="flex justify-content-between mb-3">
-                            <div>
-                              <span className="block text-500 font-medium mb-3">
-                                {t(market.marketDay)}
-                              </span>
-                              <div className="text-900 font-medium text-xl">
-                                {market.time}
+                {selectedState && selectedCity ? (
+                  <>
+                    <h2 className="mt-3">
+                      {t(
+                        `select_market_in_${cities[selectedState].find(
+                          (city) => city.value === selectedCity
+                        )?.label
+                        }`
+                      )}
+                    </h2>
+                    <div className="grid md:px-5 py-3">
+                      {filteredMarkets.length > 0 ? (
+                        filteredMarkets.map((market, index) => (
+                          <div
+                            key={index}
+                            className="col-12 md:col-6 lg:col-3 cursor-pointer"
+                            onClick={() => handleMarket(market)}
+                          >
+                            <div className="surface-0 shadow-1 p-3 border-1 border-50 border-round h-full hover:shadow-5">
+                              <div className="flex justify-content-between mb-3">
+                                <div>
+                                  <span className="block text-500 font-medium mb-3">
+                                    {t(market.marketDay)}
+                                  </span>
+                                  <div className="text-900 font-medium text-xl">
+                                    {market.time}
+                                  </div>
+                                </div>
+                                <div
+                                  className="flex align-items-center justify-content-center bg-green-100 border-round"
+                                  style={{ width: "2.5rem", height: "2.5rem" }}
+                                  onClick={() => handleLocation(market.location)}
+                                >
+                                  <i className="pi pi-map-marker text-green-500 text-xl"></i>
+                                </div>
                               </div>
-                            </div>
-                            <div
-                              className="flex align-items-center justify-content-center bg-green-100 border-round"
-                              style={{ width: "2.5rem", height: "2.5rem" }}
-                              onClick={() => handleLocation(market.location)}
-                            >
-                              <i className="pi pi-map-marker text-green-500 text-xl"></i>
+                              <div className="text-red-500 ">{t(market.name)}</div>
+                              <span className="font-medium">
+                                {t(market.address)}
+                              </span>
                             </div>
                           </div>
-                          <div className="text-red-500 ">{t(market.name)}</div>
-                          <span className="font-medium">
-                            {t(market.address)}
-                          </span>
-                        </div>
-                      </div>
-                    ))
-                  ) : (
-                    <p>{t("no_markets_found")}</p>
-                  )}
-                </div>
-              </>
-            ) : (
-              <h2>Please select state and city</h2>
-            )}
-          </div>
-        </div>
-      )}
-      </>
+                        ))
+                      ) : (
+                        <p>{t("no_markets_found")}</p>
+                      )}
+                    </div>
+                  </>
+                ) : (
+                  <h2>Please select state and city</h2>
+                )}
+              </div>
+            </div>
+          )}
+        </>
       )}
     </div>
   );
