42af518d1044754d81767108f60651a9516b42d2
kapilbelure <kapil.belure@multigenesys.com>
2025-01-24 16:00:52 +0530

    bugfix : routing , footer, product categories

--- client/src/components/customer/productCategory/index.js
+++ client/src/components/customer/productCategory/index.js
@@ -3,79 +3,6 @@ import { t } from 'i18next'
 import { categoryImageMap } from './product'
 
 export default function ProductCategory() {
-  // const images = [
-  //   {
-  //     id: 0,
-  //     itemImageSrc:
-  //       'https://post.healthline.com/wp-content/uploads/2020/08/fruits-and-vegetables-thumb.jpg',
-  //     alt: 'Image 1',
-  //     firstHalfTitle: 'Fruits And',
-  //     secHalfTitle: 'Vegetables',
-  //   },
-  //   {
-  //     id: 1,
-  //     itemImageSrc:
-  //       'https://cdn.shopify.com/s/files/1/0405/5164/5352/files/banner_300x.jpg?v=1647631081',
-  //     alt: 'Image 2',
-  //     firstHalfTitle: 'Snacks',
-  //   },
-  //   {
-  //     id: 2,
-  //     itemImageSrc:
-  //       'https://pibindia.files.wordpress.com/2016/12/istock_000020447381_medium.jpg?w=1400',
-  //     firstHalfTitle: 'Pulses &',
-  //     secHalfTitle: 'Grains',
-  //     alt: 'Image 3',
-  //   },
-
-  //   {
-  //     id: 3,
-  //     itemImageSrc:
-  //       'https://static.vecteezy.com/system/resources/thumbnails/007/558/975/small/nature-organic-product-logo-with-hand-and-leaf-design-template-free-vector.jpg',
-  //     alt: 'Image 4',
-  //     firstHalfTitle: 'Organic',
-  //     secHalfTitle: 'Products',
-  //   },
-
-  //   {
-  //     id: 4,
-  //     itemImageSrc:
-  //       'https://domf5oio6qrcr.cloudfront.net/medialibrary/9685/iStock-544807136.jpg',
-  //     alt: 'Image 5',
-  //     secHalfTitle: 'Products',
-  //   },
-  //   {
-  //     id: 5,
-  //     itemImageSrc:
-  //       'https://shilpaahuja.com/wp-content/uploads/2018/03/latest-saree-designs-trends-style-fashion-indian-designer-summer-2018.jpg',
-  //     alt: 'Image 6',
-  //     firstHalfTitle: 'Fashion',
-  //   },
-  //   {
-  //     id: 6,
-  //     itemImageSrc:
-  //       'https://m.media-amazon.com/images/I/91gbfULvW0L._AC_SL1500_.jpg',
-  //     alt: 'Image 7',
-  //     firstHalfTitle: 'Toys & ',
-  //     secHalfTitle: 'Baby Products',
-  //   },
-  //   {
-  //     id: 7,
-  //     itemImageSrc:
-  //       'https://imgmedia.lbb.in/media/2019/08/5d596136e2f8fb4ec61e9405_1566138678272.jpg',
-  //     alt: 'Image 8',
-  //     firstHalfTitle: 'Furniture',
-  //   },
-  //   {
-  //     id: 8,
-  //     itemImageSrc:
-  //       'https://www.popoptiq.com/wp-content/uploads/2019/01/13-26-1.jpg.webp',
-  //     alt: 'Image 9',
-  //     firstHalfTitle: 'Fun & ',
-  //     secHalfTitle: 'Entertainment',
-  //   },
-  // ]
-
   const images = [
     {
       id: 0,
@@ -155,7 +82,7 @@ export default function ProductCategory() {
                 <div className='flex flex-column align-items-center justify-content-center'>
                   <div className='overflow-hidden'>
                     <div
-                      className='bg-contain mb-2 bg-center bg-no-repeat h-4rem w-4rem sm:h-10rem sm:w-10rem md:h-10rem md:w-10rem lg:h-10rem lg:w-10rem border-round-2xl'
+                      className='bg-contain bg-center bg-no-repeat h-4rem w-4rem sm:h-10rem sm:w-10rem md:h-10rem md:w-10rem lg:h-10rem lg:w-10rem border-round-2xl'
                       style={{
                         backgroundImage: `url(${image.itemImageSrc})`,
                       }}></div>
